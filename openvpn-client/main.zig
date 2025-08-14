b13e8dbd0d1075a9058668a01688b22c708ef27a
Soroosh Chabi <soroosh.chabi@gmail.com>
2025-08-14 07:35:34 +0330

    listen to status change

--- openvpn-client/main.zig
+++ openvpn-client/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const gio = @import("clibs.zig").gio;
 const credentials = @import("credentials.zig");
 const client = @import("client.zig");
 
@@ -19,13 +20,36 @@ pub fn main() !void {
     const config_path = (try config_manager.LookupConfigName(allocator, creds.config_name)).?;
     defer allocator.free(config_path);
 
-    const session = try session_manager.createNewTunnel(allocator, config_path);
+    var session = try session_manager.createNewTunnel(allocator, config_path);
     defer session.deinit();
     defer session.disconnect() catch {};
 
-    try session.forwardLogs();
+    const main_loop = gio.g_main_loop_new(null, gio.FALSE) orelse return;
+    defer gio.g_main_loop_unref(main_loop);
 
-    try session.connect();
+    _ = gio.g_unix_signal_add(std.os.linux.SIG.INT, sigIntHandler, main_loop);
+
+    try session.listenToStatusChange(statusChangedHandler, null);
+
+    const thread = try std.Thread.spawn(.{}, client.Session.connect, .{session});
+    defer thread.join();
+
+    gio.g_main_loop_run(main_loop);
+}
+
+fn sigIntHandler(user_data: ?*anyopaque) callconv(.c) gio.gboolean {
+    std.debug.print("quitting...\n", .{});
+    gio.g_main_loop_quit(@ptrCast(user_data));
+    return gio.FALSE;
+}
+
+fn statusChangedHandler(_: *gio.GDBusProxy, _: [*:0]u8, _: [*:0]u8, parameters: *gio.GVariant, _: ?*anyopaque) callconv(.c) void {
+    var major: u32 = undefined;
+    var minor: u32 = undefined;
+    var message: [*:0]u8 = undefined;
+    gio.g_variant_get(parameters, "(uus)", &major, &minor, &message);
+    defer gio.g_free(message);
+    std.debug.print("Status Changed. {d}.{d}: {s}\n", .{ major, minor, message });
 }
 
 fn generateTotp(allocator: std.mem.Allocator, totp_secret: []const u8) ![]u8 {
